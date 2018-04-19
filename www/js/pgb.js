5b6fc73c9f3ec89c302770cba2d931dbe50e41f3
Szajrii <michal05v@tlen.pl>
2018-04-19 16:58:14 +0200

    fixing firebase attempt 12

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,6 +6,24 @@ function onDeviceReady() {
 	
     console.log(navigator.contacts);
     window.open = cordova.InAppBrowser.open;
+
+    firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token.
+    // You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  alert(errorMessag); 
+  console.log(errorCode);
+  console.log(errorMessag);
+});
  }
  
 function deviceInfo() {
